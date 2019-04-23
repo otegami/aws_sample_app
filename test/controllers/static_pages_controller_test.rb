31fece1e56ec7309305044715c2527039b707a71
Otegami <a.s.takuya1026@gmai.com>
2019-04-23 11:06:13 +0000

    Create UserController Action :new

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,28 +7,28 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end  
   
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home|Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help|Ruby on Rails Tutorial Sample App"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About|Ruby on Rails Tutorial Sample App"
   end  
   
   #create test code /static_pages/contact
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact|Ruby on Rails Tutorial Sample App"
   end  
 
 end
