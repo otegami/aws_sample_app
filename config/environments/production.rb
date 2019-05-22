233904218ea166bdcd3ff60293bc4c68801b5a0c
otegami <[a.s.takuya1026@gmail.com]>
2019-05-22 00:13:25 +0000

    Change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
    config.action_mailer.raise_delivery_errors = false
    config.action_mailer.raise_delivery_errors = true
    config.action_mailer.delivery_method = :smtp
-   host = 'boiling-falls-48129.herokuapp.com'
+   host = 'boiling-bayou-28746.herokuapp.com'
    config.action_mailer.default_url_options = { host: host }
    ActionMailer::Base.smtp_settings = {
      :address        => 'smtp.sendgrid.net',
