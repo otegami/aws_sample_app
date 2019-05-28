542e14c0aa2d01078d9eecf0940b9628593d1cea
otegami <[a.s.takuya1026@gmail.com]>
2019-05-28 15:01:36 +0000

    create shouw action about microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,4 +26,10 @@
                 activated:true,
                 activated_at: Time.zone.now
               )
-  end  
+  end
+  
+  users = User.order(:created_at).take(6)
+  50.times do
+    content = Faker::Lorem.sentence(5)
+    users.each { |user| user.microposts.create(content: content) }
+  end
