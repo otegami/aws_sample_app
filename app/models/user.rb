cb6455b8b01c75f890fffaa14579f8ee15e8eb0a
otegami <[a.s.takuya1026@gmail.com]>
2019-06-01 12:58:38 +0000

    create feed function

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,6 +62,10 @@ class User < ApplicationRecord
   # return true when time is expired about password's reset
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  
+  def feed
+    Micropost.where("user_id = ?", id)
   end  
   
 private
