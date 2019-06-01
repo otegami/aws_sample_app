3f6bd3fc8450a91f1bfdf63b03a73b97b45a8810
otegami <[a.s.takuya1026@gmail.com]>
2019-06-01 12:34:45 +0000

    Do refactoring about home page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   
   def create
-    @microposts = current_user.microposts.build(micropost_params)
+    @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
