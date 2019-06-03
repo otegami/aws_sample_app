a536ba1a746a63d6cfa8755d36ae223c26e3cd73
[otegami] <[a.s.takuya1026@gmail.com]>
2019-06-03 04:05:55 +0000

    create models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy                                
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
+  
   before_save :downcase_email
   before_create :create_activation_digest
+  
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -66,6 +76,21 @@ class User < ApplicationRecord
   
   def feed
     Micropost.where("user_id = ?", id)
+  end  
+  
+    # follow user
+  def follow(other_user)
+    following << other_user
+  end
+  
+  # unfollow user
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end  
+  
+  # whether follow or not
+  def following?(other_user)
+    following.include?(other_user)
   end  
   
 private
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,12 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end  
+  
   resources :account_activations, only: [:edit]
   resources :password_resets, except: [:index, :show, :destroy]
   resources :microposts, only: [:create, :destroy]
